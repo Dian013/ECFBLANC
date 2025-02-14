4623d2bfdbc39c4859f2361526f23508a9f8e1ba
Dian <mavrodiev.dian@yahoo.fr>
2025-02-14 16:18:25 +0100

    search

--- pages/search/search.js
+++ pages/search/search.js
@@ -22,43 +22,39 @@ function search(word){
             
             const meals = data.meals;
 
-            const div = document.querySelector('#search');
-
-            div.appendChild(meals[0].strMeal)
-
-            // for (let i = 0; i < meals.length; i++){
-            //     const div = document.querySelector('#search');
+            for (let i = 0; i < meals.length; i++){
+                const div = document.querySelector('#search');
         
-            //     let img = document.createElement("img");
-            //     let h2 = document.createElement("h2");
-            //     let p_area_category = document.createElement("p");
+                let img = document.createElement("img");
+                let h2 = document.createElement("h2");
+                let p_area_category = document.createElement("p");
                 
                 
-            //     img.src = meals[i].strMealThumb;
-            //     h2.textContent = meals[i].strMeal;
-            //     p_area_category.textContent = `Area : ${meals[i].strArea}
-            //                                     Category : ${meals[i].strCategory}`;
+                img.src = meals[i].strMealThumb;
+                h2.textContent = meals[i].strMeal;
+                p_area_category.textContent = `Area : ${meals[i].strArea}
+                                                Category : ${meals[i].strCategory}`;
                 
-            //     div.appendChild(img);
-            //     div.appendChild(h2);
-            //     div.appendChild(p_area_category);
+                div.appendChild(img);
+                div.appendChild(h2);
+                div.appendChild(p_area_category);
                 
-            //     for (let j = 0; j < 15; j++){
-            //         let p_ingredient = document.createElement("p");
-            //         p_ingredient.textContent = meals[i][`strIngredient${j}`];
-            //         if (p_ingredient.textContent !== ""){
-            //             div.appendChild(p_ingredient);
-            //         }
-            //     }
+                for (let j = 0; j < 15; j++){
+                    let p_ingredient = document.createElement("p");
+                    p_ingredient.textContent = meals[i][`strIngredient${j}`];
+                    if (p_ingredient.textContent !== ""){
+                        div.appendChild(p_ingredient);
+                    }
+                }
         
-            //     for (let j = 0; j < 15; j++){
-            //         let p_measure = document.createElement("p");
-            //         p_measure.textContent = meals[i][`strMeasure${j}`]
-            //         if (p_measure.textContent !== ""){
-            //             div.appendChild(p_measure);
-            //         }
-            //     }
-            // }
+                for (let j = 0; j < 15; j++){
+                    let p_measure = document.createElement("p");
+                    p_measure.textContent = meals[i][`strMeasure${j}`]
+                    if (p_measure.textContent !== ""){
+                        div.appendChild(p_measure);
+                    }
+                }
+            }
         }
     )
 }
