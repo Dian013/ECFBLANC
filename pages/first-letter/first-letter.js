d2224d3c3888f57273a91e6e8b73f5401a288ae7
Dian <mavrodiev.dian@yahoo.fr>
2025-02-14 14:27:55 +0100

    alphabet

--- pages/first-letter/first-letter.js
+++ pages/first-letter/first-letter.js
@@ -1,15 +1,48 @@
 
 function first_letter(letter){ 
+    const div = document.querySelector('#first-letter')
+
+    div.innerHTML = "";
+
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
         .then((res) => res.json())
         .then((data) => {
+            
             const meals = data.meals;
-            const div = document.querySelector('#first-letter')
 
-            for (let i = 0; i < meals.length; i++) {
+            for (let i = 0; i < meals.length; i++){
+                const div = document.querySelector('#first-letter');
+        
+                let img = document.createElement("img");
                 let h2 = document.createElement("h2");
-                h2.textContent = meals[i].strCategory;
+                let p_area_category = document.createElement("p");
+                
+                
+                img.src = meals[i].strMealThumb;
+                h2.textContent = meals[i].strMeal;
+                p_area_category.textContent = `Area : ${meals[i].strArea}
+                                                Category : ${meals[i].strCategory}`;
+                
+                div.appendChild(img);
                 div.appendChild(h2);
+                div.appendChild(p_area_category);
+                
+                for (let j = 0; j < 15; j++){
+                    let p_ingredient = document.createElement("p");
+                    p_ingredient.textContent = meals[i][`strIngredient${j}`];
+                    if (p_ingredient.textContent !== ""){
+                        div.appendChild(p_ingredient);
+                    }
+                }
+        
+                for (let j = 0; j < 15; j++){
+                    let p_measure = document.createElement("p");
+                    p_measure.textContent = meals[i][`strMeasure${j}`]
+                    if (p_measure.textContent !== ""){
+                        div.appendChild(p_measure);
+                    }
+                }
             }
-        });
+        }
+    )
 }
