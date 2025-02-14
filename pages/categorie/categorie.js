a8bdec1c6f8edf9fd3510cef2e3ec31c5e771323
Dian <mavrodiev.dian@yahoo.fr>
2025-02-14 18:48:49 +0100

    lien todo

--- pages/categorie/categorie.js
+++ pages/categorie/categorie.js
@@ -10,8 +10,16 @@ fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categorie}`)
         for (let i = 0; i < meals.length; i++) {
             let h2 = document.createElement("h2");
             let img = document.createElement("img")
+            let meal_links = document.createElement("a")
+
+            const id = meals[i].idMeal
+
             img.src = meals[i].strMealThumb
             h2.textContent = meals[i].strMeal;
+
+            meal_links 
+            //TODO 
+
             div.appendChild(img);
             div.appendChild(h2);
         }
