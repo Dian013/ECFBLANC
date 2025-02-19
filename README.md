f8f5212445e3af4ae112dbc5cc7b04d289e660ad
Dian <mavrodiev.dian@yahoo.fr>
2025-02-19 13:29:57 +0100

    html css presque terminé

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 
 Faire au moins 1 test
 Des try catch
+
+correction pour les images, migas n'a pas d'image
+
+message erreur si la lettre ne mene null part
--- pages/meal/meal.js
+++ pages/meal/meal.js
@@ -16,40 +16,53 @@ meal(ID)
 
 function meal(ID){
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${ID}`)
-        .then((res) => res.json())
-        .then((data) => {
-            const meals = data.meals;
-            const div = document.querySelector('#meal')
+    .then((res) => res.json())
+    .then((data) => {
+        const meals = data.meals;
+        const div = document.querySelector('#meal')
+        
 
-            let img = document.createElement("img")
-            let h2 = document.createElement("h2");
-            let p_area_category = document.createElement("p");
-            
+        let img = document.createElement("img")
+        let h2 = document.createElement("h2");
+        let p_area = document.createElement("p");
+        let p_category = document.createElement("p");
+        let div_Ingredient = document.createElement("div");
+        let div_Measure = document.createElement("div");
+        let div_Ingredient_Measure = document.createElement("div");
 
-            img.src = meals[0].strMealThumb
-            h2.textContent = meals[0].strMeal;
-            p_area_category.textContent = `Area : ${meals[0].strArea}
-                            Category : ${meals[0].strCategory}`;
-            
-            div.appendChild(img);
-            div.appendChild(h2);
-            div.appendChild(p_area_category);
-            
-            for (let i = 0; i < 15; i++){
-                let p_ingredient = document.createElement("p");
-                p_ingredient.textContent = meals[0][`strIngredient${i}`]
-                if (p_ingredient.textContent !== ""){
-                    div.appendChild(p_ingredient);
-                }
+        div_Ingredient_Measure.id = "div_Ingredient_Measure"
+
+        img.src = meals[0].strMealThumb
+        h2.textContent = meals[0].strMeal;
+        p_area.textContent = `Area : ${meals[0].strArea}`;
+        p_category.textContent = `Category : ${meals[0].strCategory}`;
+        
+
+
+        
+        for (let i = 0; i < 15; i++){
+            let p_ingredient = document.createElement("p");
+            p_ingredient.textContent = meals[0][`strIngredient${i}`]
+            if (p_ingredient.textContent !== ""){
+                div_Ingredient.appendChild(p_ingredient);
             }
+        }
 
-            for (let i = 0; i < 15; i++){
-                let p_measure = document.createElement("p");
-                p_measure.textContent = meals[0][`strMeasure${i}`]
-                if (p_measure.textContent !== ""){
-                    div.appendChild(p_measure);
-                }
+        for (let i = 0; i < 15; i++){
+            let p_measure = document.createElement("p");
+            p_measure.textContent = meals[0][`strMeasure${i}`]
+            if (p_measure.textContent !== ""){
+                div_Measure.appendChild(p_measure);
             }
-        }   
-    )
+        }        
+        
+        div.appendChild(img);
+        div.appendChild(h2);
+        div.appendChild(p_area);
+        div.appendChild(p_category);
+            div_Ingredient_Measure.appendChild(div_Ingredient)
+            div_Ingredient_Measure.appendChild(div_Measure)
+        div.appendChild(div_Ingredient_Measure)
+
+    });
 }
